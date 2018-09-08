9d6c5389d44ac444cc290939f60bf870161aab34
SkyeN <skye@dames.es>
2018-09-08 10:12:11 -0700

    add first method to pass first test

--- ClockAngle/Models/ClockAngle.cs
+++ ClockAngle/Models/ClockAngle.cs
@@ -1,10 +1,14 @@
+using System.Linq;
+using System;
 namespace ClockAngle.Models
 {
     public class Clock
     {
         public static int FindAngle(int hour, int minute)
         {
-            return 1;
+            int hourLocation = hour * 5;
+            int degreeDiff = Math.Abs(hourLocation-minute)*6;
+            return degreeDiff;
         }
     }
 }
