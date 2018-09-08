23f69c187e29da371200530089a4b25a22f8364d
SkyeN <skye@dames.es>
2018-09-08 10:38:33 -0700

    make method and test to check input validity

--- ClockAngle.Tests/ModelTests/ClockAngleTests.cs
+++ ClockAngle.Tests/ModelTests/ClockAngleTests.cs
@@ -7,18 +7,25 @@ namespace ClockAngle.TestTools
     public class ClockAngleTest
     {
         [TestMethod]
-        public void FindAngle_SameHandsLocation_0()
+        public void FindAngle_SameHandsLocation_string()
         {
             int hour = 3;
             int minute = 15;
-            Assert.AreEqual(0,Clock.FindAngle(hour,minute));
+            Assert.AreEqual("Distance between the hands is: 0",Clock.FindAngle(hour,minute));
         }
         [TestMethod]
-        public void FindAngle_ReturnAngleBetweenHands_30()
+        public void FindAngle_ReturnAngleBetweenHands_string()
         {
-            int hour = 6;
-            int minute = 0;
-            Assert.AreEqual(180,Clock.FindAngle(hour,minute));
+            int hour = 12;
+            int minute = 15;
+            Assert.AreEqual("Distance between the hands is: 90",Clock.FindAngle(hour,minute));
+        }
+        [TestMethod]
+        public void ValidityCheck_InvalidTime_False()
+        {
+            int hour = 13;
+            int minute = 60;
+            Assert.IsFalse(Clock.ValidityCheck(hour,minute));
         }
     }
 }
